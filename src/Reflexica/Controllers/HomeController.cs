62933de6baede4a32ed6f6b23bddf179fb771577
ambr07 <gaurav.ambre@crest.in>
2016-11-24 15:00:15 +0530

    - Implemented upload file via drag n drop option - Refactored code

--- src/Reflexica/Controllers/HomeController.cs
+++ src/Reflexica/Controllers/HomeController.cs
@@ -7,46 +7,41 @@ namespace Reflexica.Controllers
 {
     public class HomeController : Controller
     {
-      private const string TEMP_PATH = @"D:\Gaurav";
-        // GET: Home
-        public ActionResult Index()
-        {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+      private const string TEMP_PATH = @"D:\Reflexica";
 
-            return View();
-        }
+      public ActionResult Index()
+      {
+          ViewBag.Message = "Welcome to ASP.NET MVC!";
 
-        //public ActionResult About()
-        //{
-        //    return View();
-        //}
+          return View();
+      }
 
-          [HttpPost]
-        public ActionResult UploadFiles(IEnumerable files)
-        {
-            foreach (HttpPostedFileBase file in files)
-            {
-                string filePath = Path.Combine(TEMP_PATH, file.FileName);
-                System.IO.File.WriteAllBytes(filePath, ReadData(file.InputStream));
-            }
+      [HttpPost]
+      public ActionResult UploadFiles(IEnumerable files)
+      {
+          foreach (HttpPostedFileBase file in files)
+          {
+              string filePath = Path.Combine(TEMP_PATH, file.FileName);
+              System.IO.File.WriteAllBytes(filePath, ReadData(file.InputStream));
+          }
 
-            return Json("All files have been successfully stored.");
-        }
+          return Json("All files have been successfully stored.");
+      }
 
-        private byte[] ReadData(Stream stream)
-        {
-            byte[] buffer = new byte[16 * 1024];
+      private byte[] ReadData(Stream stream)
+      {
+          byte[] buffer = new byte[16 * 1024];
 
-            using (MemoryStream ms = new MemoryStream())
-            {
-                int read;
-                while ((read = stream.Read(buffer, 0, buffer.Length)) > 0)
-                {
-                    ms.Write(buffer, 0, read);
-                }
+          using (MemoryStream ms = new MemoryStream())
+          {
+              int read;
+              while ((read = stream.Read(buffer, 0, buffer.Length)) > 0)
+              {
+                  ms.Write(buffer, 0, read);
+              }
 
-                return ms.ToArray();
-            }
-        }
+              return ms.ToArray();
+          }
+      }
     }
 }
---
+++ src/Reflexica/Scripts/Main.js
@@ -0,0 +1,30 @@
+﻿$(function () {
+                    $('#drop-area').filedrop({
+                        url: '/Home/UploadFiles',
+                        paramname: 'files',
+                        dragOver: function () {
+                            $('#drop-area').css('background', 'darkcyan');
+                        },
+                        dragLeave: function () {
+                            $('#drop-area').css('background', 'burlywood');
+                        },
+                        drop: function () {
+                            $('#drop-area').css('background', 'lightgrey');
+                        },
+                        afterAll: function () {
+                            $('#drop-area').html('The file(s) have been uploaded successfully!');
+                        },
+                        uploadFinished: function (i, file, response, time) {
+                            $('#upload-result').append('<li>' + file.name + '</li>');
+                        }
+                    });
+  
+                    $("#insert-text").click(function () {
+                        $("#input-text").show();
+                        $("#drop-area").hide();
+                    });
+                    $("#drop-file").click(function () {
+                        $("#input-text").hide();
+                        $("#drop-area").show();
+                    });
+});
