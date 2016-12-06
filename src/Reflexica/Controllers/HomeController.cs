dbed6bd2c70f96f2199ea7f2953272836cd4ed4b
ambr07 <gaurav.ambre@crest.in>
2016-12-06 17:11:09 +0530

    - Created client

--- src/Reflexica/Controllers/HomeController.cs
+++ src/Reflexica/Controllers/HomeController.cs
@@ -1,7 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Reflexica.Models;
 
 namespace Reflexica.Controllers
 {
@@ -9,11 +16,13 @@ namespace Reflexica.Controllers
     {
       private const string TEMP_PATH = @"D:\Reflexica";
 
+      private const string apiUrl = "http://192.168.84.140/cgi-enabled/Reflexica.cgi";
+
+
       public ActionResult Index()
       {
-          ViewBag.Message = "Welcome to ASP.NET MVC!";
-
-          return View();
+        Journal journal = new Journal();
+          return View(journal);
       }
 
       [HttpPost]
@@ -27,6 +36,83 @@ namespace Reflexica.Controllers
 
           return Json("All files have been successfully stored.");
       }
+
+      //[HttpPost]
+      public ActionResult Submit(Journal journals)
+      {
+        try
+        {
+          ClientCall();
+          return  new JsonResult();
+        }
+        catch (Exception)
+        {
+          throw;
+        }
+      }
+
+      //...POST working fine
+      private  void ClientCall()
+      {
+        try
+        {
+          var client = new HttpClient();
+          var fileInfo = new FileInfo("D:\\index.html");
+          var fileContent = new StreamContent(fileInfo.OpenRead());
+          fileContent.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
+          {
+            Name = "\"file\"",
+            FileName = "\"" + fileInfo.Name + "\""
+          };
+          fileContent.Headers.ContentType =
+              MediaTypeHeaderValue.Parse(MimeMapping.GetMimeMapping(fileInfo.Name));
+          // This is the postdata
+          var postData = new List<KeyValuePair<string, string>>();
+          postData.Add(new KeyValuePair<string, string>("file", fileContent.ToString()));
+          postData.Add(new KeyValuePair<string, string>("style", "Basic"));
+          postData.Add(new KeyValuePair<string, string>("Client ", "Springer"));
+          postData.Add(new KeyValuePair<string, string>("style", "Basic"));
+          postData.Add(new KeyValuePair<string, string>("Language ", "En"));
+          postData.Add(new KeyValuePair<string, string>("Format", "color"));
+
+         // HttpContent content = new FormUrlEncodedContent(postData);
+          MultipartFormDataContent form = new MultipartFormDataContent();
+          form.Add(new StringContent("Basic"), "style");
+          form.Add(new StringContent("Springer"), "Client");
+          form.Add(new StringContent("Basic"), "style");
+          form.Add(new StringContent("En"), "Language");
+          form.Add(new StringContent("color"), "Format");
+          client.PostAsync("http://192.168.84.140/cgi-enabled/Reflexica.cgi", form).ContinueWith(
+              (postTask) =>
+              {
+                var res = postTask.Result.Content.ReadAsStringAsync();
+                postTask.Result.EnsureSuccessStatusCode();
+              });
+        }
+        catch (Exception ex)
+        {
+          throw new Exception(ex.Message);
+        }
+        
+      }
+      //...GET working fine
+      //private async Task ClientCall()
+      //{
+      //  using (HttpClient client = new HttpClient())
+      //  using (HttpResponseMessage response = await client.GetAsync("http://192.168.84.140/cgi-enabled/Download.cgi?File= /var/www/html/cgi-enabled/OUTPUT/508_2016_551_Article.docm-org.html"))
+      //  using (HttpContent content = response.Content)
+      //  {
+      //    // ... Read the string.
+      //    string result = await content.ReadAsStringAsync();
+
+      //    // ... Display the result.
+      //    if (result != null &&
+      //        result.Length >= 50)
+      //    {
+      //      Console.WriteLine(result.Substring(0, 50) + "...");
+      //    }
+      //  }
+      //}
 
       private byte[] ReadData(Stream stream)
       {
